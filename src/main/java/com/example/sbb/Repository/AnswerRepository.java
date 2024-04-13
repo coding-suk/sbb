a4c1046db2dcbb7eaa7dce41e3ae996a9d2fd015
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-04-13 22:13:59 +0900

    Add interface

---
+++ src/main/java/com/example/sbb/Repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.example.sbb.Repository;
+
+import com.example.sbb.Entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
---
+++ src/main/java/com/example/sbb/Repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.example.sbb.Repository;
+
+import com.example.sbb.Entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
