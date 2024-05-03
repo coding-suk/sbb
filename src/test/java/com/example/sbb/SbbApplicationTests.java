52f1583d7e24b292551db47803effd63e49b45ec
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-05-03 19:32:53 +0900

    Modify Test SbbApplicationTests.java

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.sbb;
 
+import com.example.sbb.Entity.Question;
+import com.example.sbb.Repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@Test
-	void contextLoads() {
+	void TestJpa() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q =oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
 	}
 
 }
