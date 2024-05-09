086461624277bf27a5bbb2c4217c2824c4ef017e
developer1 <dev1@email.com>
2024-05-10 01:54:10 +0900

    Add Questioncontroller Implement the site.

--- src/main/java/com/example/sbb/Controller/QuestionController.java
+++ src/main/java/com/example/sbb/Controller/QuestionController.java
@@ -1,16 +1,27 @@
 package com.example.sbb.Controller;
 
+import org.springframework.ui.Model;
+import com.example.sbb.Entity.Question;
+import com.example.sbb.Repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
+    //@ResponseBody
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 
 }
