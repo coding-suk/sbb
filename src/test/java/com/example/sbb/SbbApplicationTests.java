3942c6fed33ef474b5bb86c6de60ed33090cd389
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-05-09 17:51:43 +0900

    Modify db manage

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.sbb;
 
+import com.example.sbb.Entity.Answer;
 import com.example.sbb.Entity.Question;
+import com.example.sbb.Repository.AnswerRepository;
 import com.example.sbb.Repository.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,18 +17,26 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 @SpringBootTest
 class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Transactional
 	@Test
 	void TestJpa() {
-		Optional<Question> oq = this.questionRepository.findById(1);
+		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
-		Question q =oq.get();
-		q.setSubject("수정된 제목");
-		this.questionRepository.save(q);
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1,answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		}
 	}
 
-}
