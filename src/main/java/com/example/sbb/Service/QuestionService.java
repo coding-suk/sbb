4f1ccb0357a8a64adea0b56b39ebfd7d3bbc33bc
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-05-10 18:49:13 +0900

    Add QuestionService.java

---
+++ src/main/java/com/example/sbb/Service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.example.sbb.Service;
+
+import com.example.sbb.Entity.Question;
+import com.example.sbb.Repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
