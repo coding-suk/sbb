4126822a13b4d61d8a9e3b0845d141b151494f36
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-05-16 21:48:02 +0900

    Modify QuestionController.java

--- src/main/java/com/example/sbb/Controller/QuestionController.java
+++ src/main/java/com/example/sbb/Controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.example.sbb.Controller;
 
+import com.example.sbb.Exception.DataNotFoundException;
 import com.example.sbb.Service.QuestionService;
 import org.springframework.ui.Model;
 import com.example.sbb.Entity.Question;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ import java.util.List;
 public class QuestionController {
 
     private final QuestionService questionService;
+    private final QuestionRepository questionRepository;
 
     @GetMapping("/list")
     //@ResponseBody
@@ -33,5 +36,14 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
