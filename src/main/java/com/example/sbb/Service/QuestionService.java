639bf012bf5453e7bbdd866755d3bf171c5162dd
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-07-11 21:47:47 +0900

    Modify question page arrang

--- src/main/java/com/example/sbb/Service/QuestionService.java
+++ src/main/java/com/example/sbb/Service/QuestionService.java
@@ -3,8 +3,13 @@ package com.example.sbb.Service;
 import com.example.sbb.Entity.Question;
 import com.example.sbb.Repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import com.example.sbb.Exception.DataNotFoundException;
@@ -28,6 +33,13 @@ public class QuestionService {
         else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public void create(String subject, String content) {
