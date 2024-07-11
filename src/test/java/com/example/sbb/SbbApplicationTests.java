8a4c1a3e94a3dd0c9f13c3e23749d192a3479d65
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-07-11 21:35:47 +0900

    Add paging practice 300

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -4,39 +4,23 @@ import com.example.sbb.Entity.Answer;
 import com.example.sbb.Entity.Question;
 import com.example.sbb.Repository.AnswerRepository;
 import com.example.sbb.Repository.QuestionRepository;
+import com.example.sbb.Service.QuestionService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.Optional;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
 @SpringBootTest
 class SbbApplicationTests {
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
-	@Autowired
-	private AnswerRepository answerRepository;
-
-	@Transactional
 	@Test
 	void TestJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		for (int i = 0; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
 		}
 	}
-
+}
